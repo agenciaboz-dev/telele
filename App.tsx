22b70e10ce149f28175e2f7e363c96340165c4be
nandobfer <nandobfer@yahoo.com.br>
2023-08-31 11:37:44 -0300

    feat: started working picutre

--- App.tsx
+++ App.tsx
@@ -3,12 +3,20 @@ import { Alert, StyleSheet, Text, View } from "react-native"
 import { Routes } from "./src/Routes"
 import { useEffect, useState } from "react"
 import * as SplashScreen from "expo-splash-screen"
+import * as MediaLibrary from "expo-media-library"
 import { Camera } from "expo-camera"
 
 SplashScreen.preventAutoHideAsync()
 
 const App = () => {
     const [hasPermission, setHasPermission] = useState<boolean | null>(null)
+    const [galleryPermission, requestGalleryPermission] = MediaLibrary.usePermissions()
+
+    if (galleryPermission === null) {
+        requestGalleryPermission()
+    }
+    // useEffect(() => {
+    // }, [galleryPermission])
 
     useEffect(() => {
         if (hasPermission === false) {
